f0f09cf3ae67d2d6eb5d7bc6aea005dc8249ef66
Cesar Valdez <cesar.valdez@feedhenry.com>
2016-04-01 18:38:49 +0200

    merge windows branch

--- compile.sh
+++
@@ -1,5 +0,0 @@
-
-export LD_LIBRARY_PATH=$PWD #Tell the loader to search for shared libraries here.
-
-g++ -std=c++11 -I java/include/ -Ijava/include/linux/ -Ljava/jre/lib/amd64/server/ -ljvm main.cpp -o hello 
-./hello && rm hello && rm *.log
