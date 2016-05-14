f8a18ff0fdbf9232cd1219a90d5f436c243f9f88
cesar <cesarv01@yahoo.com>
2016-05-14 14:58:45 +0000

    linux development setup + improving dynamic lib loader for linux

--- macosx/LibJNI/main.cpp
+++ macosx/LibJNI/main.cpp
@@ -3,8 +3,6 @@
 //  LibJNI
 //
 //  Created by Cesar Valdez on 05/02/2016.
-//  Copyright © 2016 Cesar Valdez. All rights reserved.
-//
 
 #include "jvm_handler.h"
 #include "jvm_object.h"
@@ -12,340 +10,299 @@
 #include <stdlib.h>
 #include <cassert>
 
+#ifdef __APPLE__
+
 #import <pthread/pthread.h>
 
+std::vector<std::string> _clzpath = {
+    "/Users/cvaldez/Desktop/NWR/java/lib",
+    "/Users/cvaldez/Desktop/NWR/java/PDFHtml/bin"};
 
-std::shared_ptr<Object> test_create_pdf_obj(JVMLoader& vm){
-    try {
-        
-        Utils::isNull(vm.GetJNIEnviorment());
-        assert(vm.GetJNIEnviorment() != nullptr);
-        std::shared_ptr<Object> pdfclazz(new Object(vm, "pdf/P2HService"));
-        return pdfclazz;
-    }catch(VMError& e){
-        std::cout << e.errorMessage << std::endl;
-    }
-    
-    return nullptr;
+#else
+
+#import <pthread.h>
+
+std::vector<std::string> _clzpath = {"../../demo/lib/", "../../demo/PDFHtml/"};
+
+#endif
+
+std::shared_ptr<Object> test_create_pdf_obj(JVMLoader &vm) {
+  try {
+
+    Utils::isNull(vm.GetJNIEnviorment());
+    assert(vm.GetJNIEnviorment() != nullptr);
+    std::shared_ptr<Object> pdfclazz(new Object(vm, "pdf/P2HService"));
+    return pdfclazz;
+  } catch (VMError &e) {
+    std::cout << e.errorMessage << std::endl;
+  }
+
+  return nullptr;
 }
 
+std::shared_ptr<Object> test_create_str_buffer(JVMLoader &vm) {
+  try {
 
-std::shared_ptr<Object> test_create_str_buffer(JVMLoader& vm){
-    try {
-        
-        Utils::isNull(vm.GetJNIEnviorment());
-        assert(vm.GetJNIEnviorment() != nullptr);
-        std::shared_ptr<Object> pdfclazz(new Object(vm, "java/lang/StringBuffer"));
-        return pdfclazz;
-    }catch(VMError& e){
-        std::cout << e.errorMessage << std::endl;
-    }
-    
-    return nullptr;
+    Utils::isNull(vm.GetJNIEnviorment());
+    assert(vm.GetJNIEnviorment() != nullptr);
+    std::shared_ptr<Object> pdfclazz(new Object(vm, "java/lang/StringBuffer"));
+    return pdfclazz;
+  } catch (VMError &e) {
+    std::cout << e.errorMessage << std::endl;
+  }
+
+  return nullptr;
 }
 
-void testing_string_allocation(std::shared_ptr<Object> jobject, std::string h1) {
-    
-    std::cout << "Sleep Thread Java " << std::endl;
-    
-    
-    for(int x=0; x<5; x++){
-       StringValue p1(h1);
-        
-        std::vector<LibJNI::BaseJavaValue*> args{&p1};
-        jobject->Call<JObject>("append", args); // (java) concat + sleep
-    }
-    
-    
-    
-    auto x = jobject->Call<StringValue>("toString");
-    
-    
-    std::cout << "class name: " <<  jobject->GetClassName() << std::endl;
-    std::cout << "buffer.ToString [string] " << x.Get() << std::endl;
-    
+void testing_string_allocation(std::shared_ptr<Object> jobject,
+                               std::string h1) {
+
+  std::cout << "Sleep Thread Java " << std::endl;
+
+  for (int x = 0; x < 5; x++) {
+    StringValue p1(h1);
+
+    std::vector<LibJNI::BaseJavaValue *> args{&p1};
+    jobject->Call<JObject>("append", args); // (java) concat + sleep
+  }
+
+  auto x = jobject->Call<StringValue>("toString");
+
+  std::cout << "class name: " << jobject->GetClassName() << std::endl;
+  std::cout << "buffer.ToString [string] " << x.Get() << std::endl;
 }
-
-
-
 
 // Multi-Thread Tests
 
-
-
-
-
 void errExitEN(int err, const char *msg) {
-    printf("error: %d  message: %s", err, msg);
-    exit(EXIT_FAILURE);
+  printf("error: %d  message: %s", err, msg);
+  exit(EXIT_FAILURE);
 }
 
 static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 
+static void *DoTask(void *vm) {
 
-static void *
-DoTask(void *vm) {
-    
-    std::string *ss = nullptr;
-    int mtx_status = 0;
-    
-    
-    try{
-        
-        // printf("DoTask  --> JVM\n");
-        JVMLoader ljvm = *reinterpret_cast<JVMLoader*>(vm);
-        std::shared_ptr<Object> _pdf = test_create_pdf_obj(ljvm);
-        
-        StringValue h{"Hello "};
-        StringValue w{"World"};
-        IntValue t{1000};
-        
-        std::vector<LibJNI::BaseJavaValue*> args {&h,&w,&t};
-        
-        
-        auto ret = _pdf->Call<StringValue>("concatHeavy", args); // (java) concat + sleep
-        
-        
-        if ((mtx_status = pthread_mutex_lock(&mtx)) != 0){
-            errExitEN(mtx_status, "pthread_mutex_lock");
-        }
-        
-        std::cout << "-> " << ret.Get() << std::endl;
-        
-        
-        if ((mtx_status = pthread_mutex_unlock(&mtx)) != 0){
-            errExitEN(mtx_status, "pthread_mutex_unlock");
-        }
-        
-        
-        ss = new std::string(ret.Get()); // ret.getStringValue().c_str();
-        
-    }catch(VMError& error){
-        std::cout << " error: " << error.errorMessage << std::endl;
+  std::string *ss = nullptr;
+  int mtx_status = 0;
+
+  try {
+
+    // printf("DoTask  --> JVM\n");
+    JVMLoader ljvm = *reinterpret_cast<JVMLoader *>(vm);
+    std::shared_ptr<Object> _pdf = test_create_pdf_obj(ljvm);
+
+    StringValue h{"Hello "};
+    StringValue w{"World"};
+    IntValue t{1000};
+
+    std::vector<LibJNI::BaseJavaValue *> args{&h, &w, &t};
+
+    auto ret =
+        _pdf->Call<StringValue>("concatHeavy", args); // (java) concat + sleep
+
+    if ((mtx_status = pthread_mutex_lock(&mtx)) != 0) {
+      errExitEN(mtx_status, "pthread_mutex_lock");
     }
-    
-    
-    return (void *) ss;
-}
 
+    std::cout << "-> " << ret.Get() << std::endl;
 
-
-
-
-
-void mthread_test(JVMLoader loader, int workers){
-    
-    
-    pthread_t grid[workers];
-    
-    
-    printf("spawming threads and doing alloc+sleep \n");
-    
-    for (int i=0; i < workers; i++) {
-        int thread_err;
-        if ((thread_err = pthread_create(&grid[i], NULL, DoTask, (void *)&loader)) != 0 ) {
-            errExitEN(thread_err, "pthread_create");
-        }
-        
-        //pthread_detach(grid[i]);
+    if ((mtx_status = pthread_mutex_unlock(&mtx)) != 0) {
+      errExitEN(mtx_status, "pthread_mutex_unlock");
     }
-    
-    printf("main thread free to do more work... \n");
-    
-    
-    for (int i=0; i<workers; i++) {
-        pthread_join(grid[i],NULL);
+
+    ss = new std::string(ret.Get()); // ret.getStringValue().c_str();
+
+  } catch (VMError &error) {
+    std::cout << " error: " << error.errorMessage << std::endl;
+  }
+
+  return (void *)ss;
+}
+
+void mthread_test(JVMLoader loader, int workers) {
+
+  pthread_t grid[workers];
+
+  printf("spawming threads and doing alloc+sleep \n");
+
+  for (int i = 0; i < workers; i++) {
+    int thread_err;
+    if ((thread_err =
+             pthread_create(&grid[i], NULL, DoTask, (void *)&loader)) != 0) {
+      errExitEN(thread_err, "pthread_create");
     }
-    
+
+    // pthread_detach(grid[i]);
+  }
+
+  printf("main thread free to do more work... \n");
+
+  for (int i = 0; i < workers; i++) {
+    pthread_join(grid[i], NULL);
+  }
 }
 
-void test_sleep_thread(std::shared_ptr<Object> jobject, std::string h1, int time) {
-    
-    std::cout << "Sleep Thread Java " << std::endl;
-    
-    
-    StringValue p1(h1);
-    StringValue p2(" ->Attach");
-    IntValue p3(time);
-    
-    std::vector<LibJNI::BaseJavaValue*> args{&p1,&p2,&p3};
-    
-    jobject->LookupMethod("concatHeavy", args);
-    auto ret = jobject->Call<StringValue>("concatHeavy", args); // (java) concat + sleep
-    
-    std::cout << "class name: " <<  jobject->GetClassName() << std::endl;
-    std::cout << "concat heavy sleep [string] " << ret.Get() << std::endl;
-    
+void test_sleep_thread(std::shared_ptr<Object> jobject, std::string h1,
+                       int time) {
+
+  std::cout << "Sleep Thread Java " << std::endl;
+
+  StringValue p1(h1);
+  StringValue p2(" ->Attach");
+  IntValue p3(time);
+
+  std::vector<LibJNI::BaseJavaValue *> args{&p1, &p2, &p3};
+
+  jobject->LookupMethod("concatHeavy", args);
+  auto ret =
+      jobject->Call<StringValue>("concatHeavy", args); // (java) concat + sleep
+
+  std::cout << "class name: " << jobject->GetClassName() << std::endl;
+  std::cout << "concat heavy sleep [string] " << ret.Get() << std::endl;
 }
 
+void test_concat(std::shared_ptr<Object> jobject, std::string x,
+                 std::string y) {
+  try {
+    StringValue param1(x);
+    StringValue param2(y);
 
+    std::vector<LibJNI::BaseJavaValue *> args{&param1, &param2};
 
-
-
-
-
-void test_concat(std::shared_ptr<Object> jobject, std::string x, std::string y ){
-    try{
-        StringValue param1(x);
-        StringValue param2(y);
-        
-        std::vector<LibJNI::BaseJavaValue*> args{&param1, &param2};
-        
-        for( auto param : args ){
-            std::cout << "Params Type: " << param->GetType() << std::endl;
-            std::cout << "Value: " << param << std::endl;
-        }
-        
-        auto ret =  jobject->Call<StringValue>("concat", args);
-        
-        std::cout << "class name: " <<  jobject->GetClassName() << std::endl;
-        std::cout << "|| concat[string] " << ret.Get() << std::endl;
-        
-    }catch(VMError& e){
-        std::cout << e.errorMessage << std::endl;
+    for (auto param : args) {
+      std::cout << "Params Type: " << param->GetType() << std::endl;
+      std::cout << "Value: " << param << std::endl;
     }
+
+    auto ret = jobject->Call<StringValue>("concat", args);
+
+    std::cout << "class name: " << jobject->GetClassName() << std::endl;
+    std::cout << "|| concat[string] " << ret.Get() << std::endl;
+
+  } catch (VMError &e) {
+    std::cout << e.errorMessage << std::endl;
+  }
 }
 
-void test_add_int(std::shared_ptr<Object> jobject,  int x, int y) {
-    
-    IntValue _x(x);
-    IntValue _y(y);
-    
-    std::cout << "class name: " <<  jobject->GetClassName() << std::endl;
-    
-    std::vector<LibJNI::BaseJavaValue*> args{&_x, &_y};
-    std::cout << " add " << jobject->Call<IntValue>("add", args).Get() << std::endl;
-    
-    
+void test_add_int(std::shared_ptr<Object> jobject, int x, int y) {
+
+  IntValue _x(x);
+  IntValue _y(y);
+
+  std::cout << "class name: " << jobject->GetClassName() << std::endl;
+
+  std::vector<LibJNI::BaseJavaValue *> args{&_x, &_y};
+  std::cout << " add " << jobject->Call<IntValue>("add", args).Get()
+            << std::endl;
 }
 
-void test_add_float_overloading(std::shared_ptr<Object> object,  float x, float y) {
-    
-    FloatValue _x(x);
-    FloatValue _y(y);
-    
-    std::cout << "class name: " <<  object->GetClassName() << std::endl;
-    
-    std::vector<LibJNI::BaseJavaValue*> args{&_x, &_y};
-    std::cout << " add " << object->Call<FloatValue>("add", args).Get() << std::endl;
-    
-    
-    std::vector<LibJNI::BaseJavaValue*> empty;
-    //auto method = jobject->LookupMethod("html2pdf", args);
-    auto method = object->LookupMethod("getInts", empty);
-    std::cout << "Method-> " << method.GetReturnTypeInfo() << std::endl;
-    
-}
+void test_add_float_overloading(std::shared_ptr<Object> object, float x,
+                                float y) {
 
+  FloatValue _x(x);
+  FloatValue _y(y);
+
+  std::cout << "class name: " << object->GetClassName() << std::endl;
+
+  std::vector<LibJNI::BaseJavaValue *> args{&_x, &_y};
+  std::cout << " add " << object->Call<FloatValue>("add", args).Get()
+            << std::endl;
+
+  std::vector<LibJNI::BaseJavaValue *> empty;
+  auto method = object->LookupMethod("getInts", empty);
+  std::cout << "Method-> " << method.GetReturnTypeInfo() << std::endl;
+}
 
 void test_array(std::shared_ptr<Object> object) {
-    
-    std::cout << "Testing Array" << std::endl;
-    
-    
-    StringValue p1("<HTML> <H1> Hello World </H1> <H3> Your subtitle Here!</H3>  </HTML>");
-    
-    std::vector<LibJNI::BaseJavaValue*> args{&p1};
-    
-    //auto method = jobject->LookupMethod("html2pdf", args);
-    auto method = object->LookupMethod("html2pdf", args);
-    
-    std::cout << "Method-> " << method.GetReturnTypeInfo() << std::endl;
-    
-    
-    
-    auto ret = object->Call<ByteArrayValue>("html2pdf", args);
-    
-    // auto collection = LibJNI::Collection<signed char>(vm.GetJNIEnviorment(), ret);
-    
-    auto collection = ret.Get();
-    for(auto c: collection) {
-        printf("%c", c);
-    }
-    
-}
 
+  std::cout << "Testing Array" << std::endl;
+
+  StringValue p1(
+      "<HTML> <H1> Hello World </H1> <H3> Your subtitle Here!</H3>  </HTML>");
+
+  std::vector<LibJNI::BaseJavaValue *> args{&p1};
+
+  auto method = object->LookupMethod("html2pdf", args);
+
+  std::cout << "Method-> " << method.GetReturnTypeInfo() << std::endl;
+
+  auto ret = object->Call<ByteArrayValue>("html2pdf", args);
+
+/*
+  auto collection = ret.Get();
+  for (auto c : collection) {
+    printf("%c", c);
+  }*/
+
+  std::cout << "pdf size: " << ret.Get().size() << std::endl;
+
+}
 
 void test_int_array(std::shared_ptr<Object> object) {
-    
-    std::cout << "Testing INT Array" << std::endl;
 
-    
-    auto ret = object->Call<IntArrayValue>("getInts");
-    
-    // auto collection = LibJNI::Collection<signed char>(vm.GetJNIEnviorment(), ret);
-    
-    auto collection = ret.Get();
-    for(auto c: collection) {
-        printf("%d", c);
-    }
-    
+  std::cout << "Testing INT Array" << std::endl;
+
+  auto ret = object->Call<IntArrayValue>("getInts");
+
+  // auto collection = LibJNI::Collection<signed char>(vm.GetJNIEnviorment(),
+  // ret);
+
+  auto collection = ret.Get();
+  for (auto c : collection) {
+    printf("%d", c);
+  }
 }
-
-
 
 void test_int_array_ex(std::shared_ptr<Object> object) {
-    
-    std::cout << "Testing INT Array" << std::endl;
-    
-    
-    
-    auto ret = object->Call<IntArrayValue>("getInts");
-    
-    // auto collection = LibJNI::Collection<signed char>(vm.GetJNIEnviorment(), ret);
-    
-    auto collection = ret.Get();
-    for(auto c: collection) {
-        printf("%d", c);
-    }
-    
+
+  std::cout << "Testing INT Array" << std::endl;
+
+  auto ret = object->Call<IntArrayValue>("getInts");
+
+  // auto collection = LibJNI::Collection<signed char>(vm.GetJNIEnviorment(),
+  // ret);
+
+  auto collection = ret.Get();
+  for (auto c : collection) {
+    printf("%d", c);
+  }
 }
 
+int main() {
 
+  // mthread_test();
 
+  std::cout << "[ Starting Java Virtual Machine ]" << std::endl;
 
+  JVMLoader vm;
 
-int main(){
-    
-    //mthread_test();
-    
-    std::cout << "[ Starting Java Virtual Machine ]" << std::endl;
-    
-    JVMLoader vm;
-    
-    auto clazz = ClassPath::LocateLibraries({ "/Users/cvaldez/Desktop/NWR/java/lib",
-        "/Users/cvaldez/Desktop/NWR/java/PDFHtml/bin"}, true);
-    
-    
-    //std::cout << "-> " << clazz << std::endl;
-    
-    
+  // std::cout << "-> " << clazz << std::endl;
+
+  try {
+
+    auto clazz = ClassPath::LocateLibraries(_clzpath, true);
+
     vm.SetClassPath(clazz);
-    
+
     vm.Start();
-    
+
     std::shared_ptr<Object> _pdf = test_create_pdf_obj(vm);
-    
+
     std::shared_ptr<Object> _str_buff = test_create_str_buffer(vm);
-    
-    
-    try {
-        
-        
-        test_add_int(_pdf, 5000,5000);
-        //test_add_int(_pdf, 3,5);
-        //test_add_float_overloading(_pdf, 3.55, 1.5);
-        //test_concat(_pdf, "Kobe", "Bryant");
-        //testing_string_allocation(_str_buff, "Hellow");
-      //  test_array( _pdf);
-       // test_int_array(_pdf);
-   //     mthread_test(vm, 400);
-        
-        
-        
-    } catch (VMError& error) {
-        std::cout << error.errorMessage << std::endl;
-    }
-    
-    exit(EXIT_SUCCESS);
+
+    test_add_int(_pdf, 5000, 5000);
+    test_add_int(_pdf, 3, 5);
+    test_add_float_overloading(_pdf, 3.55, 1.5);
+    test_concat(_pdf, "Kobe", "Bryant");
+    testing_string_allocation(_str_buff, "Hellow");
+    test_array(_pdf);
+    test_int_array(_pdf);
+    mthread_test(vm, 5);
+
+  } catch (VMError &error) {
+    std::cout << error.errorMessage << std::endl;
+  }
+
+  exit(EXIT_SUCCESS);
 };
--- src/values.h
+++ src/values.h
@@ -3,7 +3,6 @@
 //  LibJNI
 //
 //  Created by Cesar Valdez on 06/04/2016.
-//  Copyright © 2016 Cesar Valdez. All rights reserved.
 //
 
 #ifndef values_hpp
@@ -18,187 +17,163 @@ class ArgumentTypeInfo;
 class Arguments;
 
 namespace LibJNI {
-    
-     // We need to check the arguments at runtime so this class create a common interface, for interacting
-     //with the Value class.
-    
-    class BaseJavaValue {
-    public:
-        // Override with the value expected by JNI for java Argument.
-        virtual std::string GetType() { throw VMError{"Type not implemented yet."}; return ""; };
-        
-        //Override with the handling behavior for Native to JNI type.
-        virtual jvalue GetJavaValue(JEnv& env) { throw VMError{"GetJavaValue not implemented yet for this type."}; };
-    };
 
+// We need to check the arguments at runtime so this class create a common
+// interface, for interacting
+// with the Value class.
 
-    
-    
-    //This class is in charge to handle the arguments and the return type. Here are implemented the basic
-    //accessors and trivials constructors.
-    
-    template <class JNIType, class NativeType>
-    class Value: public BaseJavaValue {
-    public:
-        
-        Value(std::string _type): type(_type) {};
-        
-        Value(std::string _type, NativeType _value):
-        value(_value),
-        type(_type){}
-        
-        typedef JNIType Type;
-        
-        std::string GetType() { return type; };
-        
-        void Set(JEnv& env, JNIType object) {
-            value = (NativeType) object;
-        }
-        
-        void Set(NativeType _value){
-            value = _value;
-        }
-        
-        NativeType Get(){ return value; }
-        
-        
-    protected:
-        NativeType value;
-        std::string type;
-        jvalue jniValue;
-    };
-    
-    
-    // dealing with jobjects this is just reusing the accessors.
-    struct JObject : public Value<jobject, jobject> {
-        JObject(): Value("object") {}
-    };
-    
-    
-     //   reusing accessors and mapping types < JNI, JNI >, types get cast in Set(Native),
-     //   so if you need more sophisticated treatment override that method.
-    
-    struct ObjectArray : public Value<jobject, jobjectArray> {
-        ObjectArray(): Value("object") {}
-    };
-    
-    
-    
-     // Here we override the BaseJavaValue::GetJavaValue, cause this data need some treatment.
-    
-    
-    struct IntValue : public Value<jint, int> {
+class BaseJavaValue {
+public:
+  // Override with the value expected by JNI for java Argument.
+  virtual std::string GetType() {
+    throw VMError{"Type not implemented yet."};
+    return "";
+  };
 
-        IntValue(): Value("int") {}
-        IntValue(int x): Value("int", x) {}
-        
-        jvalue GetJavaValue(JEnv& env) {
-            jniValue.i = value;
-            return jniValue;
-        }
-    };
-    
-    struct FloatValue : public Value<jfloat, float> {
+  // Override with the handling behavior for Native to JNI type.
+  virtual jvalue GetJavaValue(JEnv &env) {
+    throw VMError{"GetJavaValue not implemented yet for this type."};
+  };
+};
 
-        FloatValue(): Value("float") {}
-        FloatValue(float x): Value("float", x) {}
-        
-        jvalue GetJavaValue(JEnv& env) {
-            jniValue.f = value;
-            return jniValue;
-        }
-    };
-    
-    
-    
-     //   We override Set and GetJavaValue.
-    
-    struct StringValue : public Value<jstring, std::string> {
+// This class is in charge to handle the arguments and the return type. Here are
+// implemented the basic
+// accessors and trivials constructors.
 
-        StringValue(): Value("java.lang.String") {}
-        StringValue(std::string str): Value("java.lang.String", str) {}
-        
-        void Set(JEnv& env, jobject object) {
-            jstring tmp = (jstring) object;
-            
-            if(tmp == nullptr) value =  "Undefined";
-            
-            const char *str =env->GetStringUTFChars( (jstring)tmp , NULL );
-            value = str;
-            env->ReleaseStringUTFChars( (jstring)tmp ,str );
-        };
-        
-        jvalue GetJavaValue(JEnv& env)  {
-            jvalue javaValue;
-            javaValue.l =  env->NewStringUTF( value.c_str() );
-            return javaValue;
-        };
-        
-    };
+template <class JNIType, class NativeType> class Value : public BaseJavaValue {
+public:
+  Value(std::string _type) : type(_type){};
 
-    /*  ====== ArrayValue =======    */
+  Value(std::string _type, NativeType _value) : value(_value), type(_type) {}
 
-    
-    
-    //    ArrayValue: T
-        
-      //  this one is cool, cause it inherit for a generic type. In reality we want from this class the method
-      //  [ Set ] cause it contain a general algorithm to iterate Java native arrays and allow you to choose your
-      //  function pointer.
-     
-    
-    
-    template <typename T>
-    struct ArrayValue: public T {
-        typedef typename T::Type Type;
-        ArrayValue(std::string type): T(type) {};
-        
-        template <typename F, typename T>
-        void Set(F& fn, JEnv& env, jobject _array) {
-            Type array = (Type) _array;
-           
-            jint count = env->GetArrayLength( array );
-            this->value.resize(count);
-            
-            fn (env.get(), array , 0, count, (T*)&this->value[0] );
-        }
-    };
-    
-    
-    
-    // IntArrayValue
-     
-    //  I treat here Java native int[] arrays, so we mix [ArrayValue, Object] and then inherit their members
-   //  overriding the Set method that is the one we specialize before.
-    
-    class IntArrayValue : public ArrayValue< Value<jintArray, std::vector<int>> > {
-    public:
-        IntArrayValue(): ArrayValue("[I") {};
-        
-        void Set(JEnv& env, jobject _array) {
-			auto Interface = env->functions->GetIntArrayRegion;
-			ArrayValue::Set<decltype(Interface), jint>(Interface, env, _array);
-        };
-    };
+  typedef JNIType Type;
 
-  
-    
-    //    ByteArrayValue 
-    //  Same as before but we change the function callback.
-     
-    
-    class ByteArrayValue : public ArrayValue< Value<jbyteArray, std::vector<signed char>> > {
-    public:
-        ByteArrayValue(): ArrayValue("[B") {};
-        
-        void Set(JEnv& env, jobject _array) {
-			auto Interface = env->functions->GetByteArrayRegion;
-			ArrayValue::Set<decltype(Interface), jbyte>(Interface, env, _array);
-        };
-    };
-    
+  std::string GetType() { return type; };
+
+  void Set(JEnv &env, JNIType object) { value = (NativeType)object; }
+
+  void Set(NativeType _value) { value = _value; }
+
+  NativeType Get() { return value; }
+
+protected:
+  NativeType value;
+  std::string type;
+  jvalue jniValue;
+};
+
+// dealing with jobjects this is just reusing the accessors.
+struct JObject : public Value<jobject, jobject> {
+  JObject() : Value("object") {}
+};
+
+//   reusing accessors and mapping types < JNI, JNI >, types get cast in
+//   Set(Native),
+//   so if you need more sophisticated treatment override that method.
+
+struct ObjectArray : public Value<jobject, jobjectArray> {
+  ObjectArray() : Value("object") {}
+};
+
+// Here we override the BaseJavaValue::GetJavaValue, cause this data need some
+// treatment.
+
+struct IntValue : public Value<jint, int> {
+
+  IntValue() : Value("int") {}
+  IntValue(int x) : Value("int", x) {}
+
+  jvalue GetJavaValue(JEnv &env) {
+    jniValue.i = value;
+    return jniValue;
+  }
+};
+
+struct FloatValue : public Value<jfloat, float> {
+
+  FloatValue() : Value("float") {}
+  FloatValue(float x) : Value("float", x) {}
+
+  jvalue GetJavaValue(JEnv &env) {
+    jniValue.f = value;
+    return jniValue;
+  }
+};
+
+//   We override Set and GetJavaValue.
+
+struct StringValue : public Value<jstring, std::string> {
+
+  StringValue() : Value("java.lang.String") {}
+  StringValue(std::string str) : Value("java.lang.String", str) {}
+
+  void Set(JEnv &env, jobject object) {
+    jstring tmp = (jstring)object;
+
+    if (tmp == nullptr)
+      value = "Undefined";
+
+    const char *str = env->GetStringUTFChars((jstring)tmp, NULL);
+    value = str;
+    env->ReleaseStringUTFChars((jstring)tmp, str);
+  };
+
+  jvalue GetJavaValue(JEnv &env) {
+    jvalue javaValue;
+    javaValue.l = env->NewStringUTF(value.c_str());
+    return javaValue;
+  };
+};
+
+/*  ====== ArrayValue =======    */
+
+//    ArrayValue: T
+
+// Reusing code to iterate over ArrayType in JNI.
+
+template <typename ValueType> struct ArrayValue : public ValueType {
+  typedef typename ValueType::Type Type;
+  ArrayValue(std::string type) : ValueType(type){};
+
+  template <typename F, typename T> void Set(F &fn, JEnv &env, jobject _array) {
+    Type array = (Type)_array;
+
+    jint count = env->GetArrayLength(array);
+    this->value.resize(count);
+
+    fn(env.get(), array, 0, count, (T *)&this->value[0]);
+  }
+};
+
+// IntArrayValue
+
+//  I treat here Java native int[] arrays, so we mix [ArrayValue, Object] and
+//  then inherit their members
+//  overriding the Set method that is the one we specialize before.
+
+class IntArrayValue : public ArrayValue<Value<jintArray, std::vector<int>>> {
+public:
+  IntArrayValue() : ArrayValue("[I"){};
+
+  void Set(JEnv &env, jobject _array) {
+    auto Interface = env->functions->GetIntArrayRegion;
+    ArrayValue::Set<decltype(Interface), jint>(Interface, env, _array);
+  };
+};
+
+//    ByteArrayValue
+//  Same as before but we change the function callback.
+
+class ByteArrayValue
+    : public ArrayValue<Value<jbyteArray, std::vector<signed char>>> {
+public:
+  ByteArrayValue() : ArrayValue("[B"){};
+
+  void Set(JEnv &env, jobject _array) {
+    auto Interface = env->functions->GetByteArrayRegion;
+    ArrayValue::Set<decltype(Interface), jbyte>(Interface, env, _array);
+  };
+};
 }
-
-
 
 #endif /* values_hpp */
